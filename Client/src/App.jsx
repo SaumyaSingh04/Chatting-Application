1b26ddc6ecfeaf861d97c2991572f74101cf5ebd
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-02-19 12:52:06 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import Chat from "./chat";
 import music from "./mixkit-tile-game-reveal-960.wav"
 
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("https://chatting-application-nrzo.onrender.com");
 
 function App() {
 
